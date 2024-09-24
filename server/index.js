a45ef0906c5843e59cda1967f1a4f92a51e94ddf
Arnav-2004 <arnavrangwani@gmail.com>
2024-09-24 11:45:05 +0530

    fix url

--- server/index.js
+++ server/index.js
@@ -75,7 +75,7 @@ app.post("/login", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3306;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3001";
+const API_URL = "http://sql12.freesqldatabase.com:3306";
 
 export const signUp = async (userData) => {
   try {
